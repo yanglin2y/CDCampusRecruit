25247f2f72707cefc9a861686201e81a1498bd55
YangLin <250099799@qq.com>
2022-01-07 19:37:37 +0800

    完成了依赖配置与应届生端搭建

---
+++ applicantcloud/src/main/java/com/ycode/cdcr/ApplicantcloudApplication.java
@@ -0,0 +1,17 @@
+package com.ycode.cdcr;
+
+import com.apple.eawt.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YangLin
+ * @createTime 2022/01/07
+ */
+@SpringBootApplication
+public class ApplicantcloudApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicantcloudApplication.class,args);
+  }
+}
